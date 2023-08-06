31f7580de00528a8ba7f02f69ff8f931e91bc63c
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-08-06 23:32:04 +0800

    works

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-const bg_cover = document.getElementsById("index_bg_overlay");
+const bg_cover = document.getElementById("index_bg_overlay");
 
 const bg_image = [
     './img/01_gma29.jpg', 
-    './img/02_gha56_01.png'];
+    './img/02_gha56_01.png',
+    './img/03_SWSX_01.jpg'
+];
 
 let index = 0;
 
 setInterval(() => {
 
     bg_cover.style.backgroundImage = `url(${bg_image[index]})`;
-
     index = (index + 1) % colors.length;
     
 }, 2000);
