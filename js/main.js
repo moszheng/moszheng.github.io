3fca28bd717b324a2efb03d26d55b927c155a517
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-08-07 11:57:53 +0800

    Contact test

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ setInterval(() => {
     index = (index + 1) % bg_image.length;
     
 }, 3500);
+
+// Contact
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwzJZ2vywOdfJsfRp_I8FFWYnI3avEc5gima4S_T21h8lz9L7cS4oDh1rAbPjNp5KqedA/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
