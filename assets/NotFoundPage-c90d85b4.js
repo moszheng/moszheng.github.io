6e21d9cdc92359f4ffae44821fa7017ae0ecc03a
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-11-05 15:42:50 +0800

    [ Fix ] Loading Speed

--- assets/NotFoundPage-c90d85b4.js
+++
@@ -1,1 +0,0 @@
-import{x as t,b as o,d as n,g as e}from"./index-3b552a1c.js";const s={},_={class:"text-center notfound"},c=e("div",{class:""},[e("h1",null,"404"),e("h3",null,"Page Not Found")],-1),a=[c];function d(r,l){return o(),n("main",_,a)}const i=t(s,[["render",d]]);export{i as default};
